f20c5d7729f8acff4f0fe8277dc0a18470cf2d5a
brambaud <beram.job@gmail.com>
2018-10-27 17:55:46 +0200

    fix(quality): fix shellcheck issues

--- SublimeDrupal.sh
+++ SublimeDrupal.sh
@@ -1,11 +1,14 @@
-#!/bin/bash
+#!/usr/bin/env bash
+
+set -euo pipefail
 
 # Attempt OS detection to set path
-os=`uname`;
-phpcs=`which phpcs`;
+os=$(uname);
+# shellcheck disable=SC2230
+phpcs=$(which phpcs);
 
 # ST2 Packages directory
-if [ $os = 'Linux' ]
+if [ "${os}" = 'Linux' ]
 then
   st2Dir=~/".config/sublime-text-3/Packages/"
 else
@@ -27,10 +30,11 @@ st2Settings="$st2UserDir"Preferences.sublime-settings;
 if [ ! -d "DrupalSublimeConfig" ]; then
  git clone https://github.com/brambaud/drupal-sublime-config.git DrupalSublimeConfig;
 else
+  (
   echo "Updating plugin DrupalSublimeConfig";
   cd "DrupalSublimeConfig"
   git pull origin master
-  cd ..
+  )
 fi
 
 # Back up old PHP settings file
@@ -88,90 +92,99 @@ ln -fs "$st2Dir"DrupalSublimeConfig/Highlighter.sublime-settings "$st2UserDir"Hi
 if [ ! -d "PACKAGE CONTROL" ]; then
   git clone https://github.com/wbond/sublime_package_control.git "Package Control";
 else
+ (
  echo "Updating plugin Package Control";
  cd "Package Control"
  git pull origin master
- cd ..
+ )
 fi
 
 # BracketHighlighter
 if [ ! -d "BracketHighlighter" ]; then
  git clone https://github.com/facelessuser/BracketHighlighter.git BracketHighlighter;
  else
+ (
   echo "Updating plugin BracketHighlighter";
   cd "BracketHighlighter"
   git pull origin master
-  cd ..
+  )
 fi
 
 # Highlighter
 if [ ! -d "Highlighter" ]; then
  git clone https://github.com/bluegray/Highlighter.git Highlighter;
  else
+ (
   echo "Updating plugin Highlighter";
   cd "Highlighter"
   git pull origin master
-  cd ..
+  )
 fi
 
 # ApplySyntax
 if [ ! -d "ApplySyntax" ]; then
  git clone https://github.com/facelessuser/ApplySyntax.git ApplySyntax;
  else
+ (
   echo "Updating plugin Apply Syntax";
   cd "ApplySyntax"
   git pull origin master
-  cd ..
+  )
 fi
 
 # Pretty YAML
 if [ ! -d "PrettyYAML" ]; then
  git clone https://github.com/aukaost/SublimePrettyYAML.git PrettyYAML;
  else
+ (
   echo "Updating plugin PrettyYAML";
   cd "PrettyYAML"
   git pull origin master
-  cd ..
+  )
 fi
 
 # DocBlockr
 if [ ! -d "DocBlockr" ]; then
  git clone https://github.com/spadgos/sublime-jsdocs.git DocBlockr;
 else
+  (
   echo "Updating plugin DocBlockr";
   cd "DocBlockr"
   git pull origin master
-  cd ..
+  )
 fi
 
 # GotoDrupalAPI
 if [ ! -d "Sublime-Text-2-Goto-Drupal-API" ]; then
  git clone https://github.com/BrianGilbert/Sublime-Text-2-Goto-Drupal-API.git;
 else
+  (
   echo "Updating plugin Sublime-Text-2-Goto-Drupal-API";
   cd "Sublime-Text-2-Goto-Drupal-API"
   git pull origin master
-  cd ..
+  )
 fi
 
 # Drupal Sublime Snippets
 if [ ! -d "DrupalSublimeSnippets" ]; then
  git clone https://github.com/juhasz/drupal_sublime-snippets.git DrupalSublimeSnippets;
 else
+  (
   echo "Updating plugin DrupalSublimeSnippets";
   cd "DrupalSublimeSnippets"
   git pull origin master
-  cd ..
+  )
 fi
 
 # DrupalCodingStandard Fork
 if [ ! -d "DrupalCodingStandard" ]; then
  git clone https://github.com/rypit/DrupalCodingStandard.git DrupalCodingStandard;
 else
+  (
   echo "Updating plugin DrupalCodingStandard";
   cd "DrupalCodingStandard"
   git pull origin master
-  cd ..
+  )
 fi
 
 #PHP Syntax Checker
@@ -188,40 +201,44 @@ fi
 if [ ! -d "sublime-text-2-goto-documentation" ]; then
  git clone https://github.com/kemayo/sublime-text-2-goto-documentation;
 else
+  (
   echo "Updating plugin sublime-text-2-goto-documentation";
   cd "sublime-text-2-goto-documentation"
   git pull origin master
-  cd ..
+  )
 fi
 
 # Synced SideBar
 if [ ! -d "SyncedSideBar" ]; then
  git clone git://github.com/sobstel/SyncedSideBar;
 else
+  (
   echo "Updating plugin SyncedSideBar";
   cd "SyncedSideBar"
   git pull origin master
-  cd ..
+  )
 fi
 
 # Traling Spaces
 if [ ! -d "TrailingSpaces" ]; then
  git clone https://github.com/SublimeText/TrailingSpaces.git;
 else
+  (
   echo "Updating plugin TrailingSpaces";
   cd "TrailingSpaces"
   git pull origin master
-  cd ..
+  )
 fi
 
 # Drupal AutoComplete
 if [ ! -d "st2-drupal-autocomplete" ]; then
  git clone https://github.com/tanc/st2-drupal-autocomplete.git;
 else
+  (
   echo "Updating plugin st2-drupal-autocomplete";
   cd "st2-drupal-autocomplete"
   git pull origin master
-  cd ..
+  )
 fi
 
 # To-Do: Test other version  right now takes to much time
@@ -239,82 +256,90 @@ fi
 if [ ! -d "sublime-text-2-git" ]; then
  git clone https://github.com/kemayo/sublime-text-2-git.git;
 else
+  (
   echo "Updating plugin Git";
   cd "sublime-text-2-git"
   git pull origin master
-  cd ..
+  )
 fi
 
 # GitGutter
 if [ ! -d "GitGutter" ]; then
  git clone https://github.com/jisaacks/GitGutter.git;
 else
+  (
   echo "Updating plugin GitGutter";
   cd "GitGutter"
   git pull origin master
-  cd ..
+  )
 fi
 
 # SideBarGit
 if [ ! -d "SideBarGit" ]; then
  git clone https://github.com/SublimeText/SideBarGit.git;
 else
+  (
   echo "Updating plugin SideBarGit";
   cd "SideBarGit"
   git pull origin master
-  cd ..
+  )
 fi
 
 # SublimeLinter
 if [ ! -d "SublimeLinter" ]; then
  git clone https://github.com/SublimeLinter/SublimeLinter.git
 else
+  (
   echo "Updating plugin SublimeLinter";
   cd "SublimeLinter"
   git pull origin master
-  cd ..
+  )
 fi
 
 # SASS
 if [ ! -d "SASS" ]; then
  git clone ghttps://github.com/nathos/sass-textmate-bundle.git SASS;
 else
+  (
   echo "Updating plugin SASS";
   cd "SASS"
   git pull origin master
-  cd ..
+  )
 fi
 
 # CssSnippets
 if [ ! -d "CssSnippets" ]; then
  git clone https://github.com/P233/Emmet-Css-Snippets-for-Sublime-Text-2.git CssSnippets;
 else
+  (
   echo "Updating plugin CssSnippets";
   cd "CssSnippets"
   git pull origin master
-  cd ..
+  )
 fi
 
 # JSLint
 if [ ! -d "JSLint" ]; then
  git clone https://github.com/darrenderidder/Sublime-JSLint.git JSLint;
 else
+  (
   echo "Updating plugin JSLint";
   cd "JSLint"
   git pull origin master
-  cd ..
+  )
 fi
 
 #Specific plugings for MAC
-if [ $os != 'Linux' ]; then
+if [ "${os}" != 'Linux' ]; then
   # MacTerminal
   if [ ! -d "MacTerminal" ]; then
      git clone https://github.com/afterdesign/MacTerminal.git
    else
+    (
     echo "Updating plugin MacTerminal";
     cd "MacTerminal"
     git pull origin master
-    cd ..
+    )
   fi
 
    if [ -d "/Applications/iTerm.app" ]; then
@@ -334,10 +359,11 @@ fi
 if [ ! -d "Theme - Soda" ]; then
  git clone https://github.com/buymeasoda/soda-theme/ "Theme - Soda"
 else
+  (
   echo "Updating plugin Theme - Soda";
   cd "Theme - Soda"
   git pull origin master
-  cd ..
+  )
 fi
 
 # fetch specific color schemas for soda theme
